7e4d3f17c180dee2293fe5f7460693e5f90c34ed
Tetr4k <g2012ribeiro@gmail.com>
2022-10-25 23:08:14 -0300

    Fix rust template

--- main.rs
+++ main.rs
@@ -1,5 +1,11 @@
 use std::io;
 
+fn read_value<T: std::default::Default + std::str::FromStr>() -> T{
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+	return input.trim().parse().unwrap_or_default();
+}
+
 fn main(){
     
 }

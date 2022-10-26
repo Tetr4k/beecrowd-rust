ddb9d95ade998c18f6798b06bdff1c4b3518188c
Tetr4k <g2012ribeiro@gmail.com>
2022-10-26 20:02:44 -0300

    Set function to split a line

--- main.rs
+++ main.rs
@@ -1,9 +1,15 @@
 use std::io;
 
+fn read_input() -> Vec<String>{
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+    (&input).split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>()
+}
+
 fn read_value<T: std::default::Default + std::str::FromStr>() -> T{
 	let mut input = String::new();
 	io::stdin().read_line(&mut input).unwrap();
-	return input.trim().parse().unwrap_or_default();
+	input.trim().parse().unwrap_or_default()
 }
 
 fn main(){

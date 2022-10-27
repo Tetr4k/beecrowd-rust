20846c18fa517861200466bd64ee9b469f7d7a90
Tetr4k <g2012ribeiro@gmail.com>
2022-10-26 21:56:01 -0300

    Question 1014 from beecrowd
    https://www.beecrowd.com.br/judge/pt/problems/view/1014

---
+++ 1014/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn read_input() -> Vec<String>{
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+    (&input).split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>()
+}
+
+fn read_value<T: std::default::Default + std::str::FromStr>() -> T{
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+	input.trim().parse().unwrap_or_default()
+}
+
+fn main(){
+    let distancia = read_value::<i128>();
+	let total = read_value::<f64>();
+	println!("{:.3} km/l", (distancia as f64)/total);
+}

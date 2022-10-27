1a123f5b6097bf754f54b7aee5ab968038bec327
Tetr4k <g2012ribeiro@gmail.com>
2022-10-26 21:56:47 -0300

    Question 1017 from beecrowd
    https://www.beecrowd.com.br/judge/pt/problems/view/1017

---
+++ 1017/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn read_input() -> Vec<String>{
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+    (&input).split_whitespace().map(|x| x.to_string()).collect::<Vec<String>>()
+}
+
+fn read_value<T: std::default::Default + std::str::FromStr>() -> T{
+	let mut input = String::new();
+	io::stdin().read_line(&mut input).unwrap();
+	input.trim().parse().unwrap_or_default()
+}
+
+fn main(){
+    let tempo = read_value::<i128>();
+    let distancia = read_value::<i128>();
+	println!("{:.3}", ((tempo*distancia) as f64)/12.0);
+}
